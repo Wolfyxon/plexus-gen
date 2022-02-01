7dc22886b1b8dd3229387c95a0eb691a31a3e87d
WolfGames <58263600+WolfGames@users.noreply.github.com>
2022-02-01 09:59:15 +0100

    Added configurable light and rainbow mode for colors

--- src/scripts/Main.gd
+++ src/scripts/Main.gd
@@ -41,6 +41,14 @@ func _physics_process(delta):
 			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/global_size_x.value,
 			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/global_size_y.value
 		)
+		$canvas/background/Plexus/back_effects/Light2D.position = Vector2(
+			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_pos_x.value,
+			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_pos_y.value
+		)
+		$canvas/background/Plexus/back_effects/Light2D.scale = Vector2(
+			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_size.value,
+			$GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_size.value
+		)
 
 		
 		$canvas/background/Plexus.enable_circles = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/circles_enabled.pressed
@@ -49,24 +57,28 @@ func _physics_process(delta):
 		get_tree().paused = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/pause_render.pressed
 		$canvas/background/Plexus.squared = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/right_angles.pressed
 		
-		if not $canvas/background/Plexus/lineAnim.is_playing():
+		if not $GUI/menus/left/properties/ScrollContainer/VBoxContainer/line_rainbow.pressed:
 			$canvas/background/Plexus.line_color = $GUI/popups/plexus_color.color
-			
-		if not $canvas/background/Plexus/circleAnim.is_playing():
+			$canvas/background/Plexus/lineAnim.stop()
+		else:
+			$canvas/background/Plexus/lineAnim.play("rainbow")
+		
+		if not $GUI/menus/left/properties/ScrollContainer/VBoxContainer/circle_rainbow.pressed:
 			$canvas/background/Plexus.circle_color = $GUI/popups/circle_color.color
+			$canvas/background/Plexus/circleAnim.stop()
+		else:
+			$canvas/background/Plexus/circleAnim.play("rainbow")
 		$canvas/background/Plexus.modulate = $GUI/popups/modulate.color
 		$canvas/background.color = $GUI/popups/bgcolor.color
 		
-		if $GUI/menus/left/properties/ScrollContainer/VBoxContainer/line_rainbow.pressed:
-			$canvas/background/Plexus/lineAnim.play("rainbow")
+		if not $GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_rainbow.pressed:
+			pass
 		else:
-			$canvas/background/Plexus/lineAnim.stop()
+			$canvas/background/Plexus/back_effects/lightAnim.play("rainbow")
 			
-		if $GUI/menus/left/properties/ScrollContainer/VBoxContainer/circle_rainbow.pressed:
-			$canvas/background/Plexus/circleAnim.play("rainbow")
-		else:
-			$canvas/background/Plexus/circleAnim.stop()
-		
+		$canvas/background/Plexus/back_effects/Light2D.self_modulate = $GUI/popups/lightcolor.color
+			
+		$canvas/background/Plexus/back_effects/lightAnim.playback_speed = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/light_rainbow_speed.value
 		$canvas/background/Plexus/lineAnim.playback_speed = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/line_rainbow_speed.value
 		$canvas/background/Plexus/circleAnim.playback_speed = $GUI/menus/left/properties/ScrollContainer/VBoxContainer/circle_rainbow_speed.value
 		
@@ -91,7 +103,8 @@ func _physics_process(delta):
 	
 	#======
 	
-	if Input.is_action_just_pressed("test"):
+	if Input.is_action_just_pressed("settings_pause"):
+		$GUI/menus/left/properties/ScrollContainer/VBoxContainer/pause_render.pressed = not($GUI/menus/left/properties/ScrollContainer/VBoxContainer/pause_render.pressed)
 		pass
 		
 	if Input.is_action_just_pressed("fullscreen"):
@@ -133,6 +146,7 @@ func _on_btn_plexus_color_pressed():
 	$GUI/popups/modulate.visible = false
 	$GUI/popups/circle_color.visible = false
 	$GUI/popups/bgcolor.visible = false
+	$GUI/popups/lightcolor.visible = false
 
 
 func _on_btn_circle_color_pressed():
@@ -140,6 +154,7 @@ func _on_btn_circle_color_pressed():
 	$GUI/popups/modulate.visible = false
 	$GUI/popups/plexus_color.visible = false
 	$GUI/popups/bgcolor.visible = false
+	$GUI/popups/lightcolor.visible = false
 
 
 func _on_btn_modulate_pressed():
@@ -147,14 +162,22 @@ func _on_btn_modulate_pressed():
 	$GUI/popups/plexus_color.visible = false
 	$GUI/popups/circle_color.visible = false
 	$GUI/popups/bgcolor.visible = false
+	$GUI/popups/lightcolor.visible = false
 
 
 func _on_btn_bgcolor_pressed():
 	$GUI/popups/bgcolor.visible = not($GUI/popups/bgcolor.visible)
 	$GUI/popups/plexus_color.visible = false
 	$GUI/popups/circle_color.visible = false
+	$GUI/popups/lightcolor.visible = false
 	$GUI/menus/left/properties/ScrollContainer/VBoxContainer/btn_modulate.visible = false
 
+func _on_btn_light_color_pressed():
+	$GUI/popups/lightcolor.visible = not($GUI/popups/lightcolor.visible)
+	$GUI/popups/plexus_color.visible = false
+	$GUI/popups/circle_color.visible = false
+	$GUI/popups/bgcolor.visible = false
+	$GUI/menus/left/properties/ScrollContainer/VBoxContainer/btn_modulate.visible = false
 
 func _on_btn_about_pressed():
 	$GUI/popups/popup_about.popup()
@@ -204,3 +227,6 @@ func _on_btn_clear_pressed():
 
 func _on_btn_reset_pressed():
 	get_tree().change_scene("res://Main.tscn")
+
+
+
